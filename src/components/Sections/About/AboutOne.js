3240ab0ccc30bb3310f7717465c5261151ebf3f4
Nasim Md Emon <nasimmdemon@Nasims-MacBook-Air.local>
2024-06-26 01:11:35 +0600

    Home page customization 40% complete

--- src/components/Sections/About/AboutOne.js
+++ src/components/Sections/About/AboutOne.js
@@ -1,44 +1,57 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const AboutOne = () => {
-    return (
-        <section className="about-block">
-            <div className="container">
-                <div className="content rounded-2xl bg-linear-blue md:p-10 p-7">
-                    <div className="heading flex max-lg:flex-col gap-y-4 md:pb-10 pb-8 border-b border-line">
-                        <div className="w-full xl:w-5/12 lg:w-1/2">
-                            <div className="tag text-label bg-blue text-white">About Us</div>
-                            <h3 className="heading3 text-white mt-4">We solve technology challenges.</h3>
-                        </div>
-                        <div className="w-full lg:w-1/2">
-                            <div className="desc text-white">Your IT Services Partner! We offer customized IT solutions, from managed services to cloud computing and cybersecurity. Empower your business with our expertise for growth and efficiency. </div>
-                            <Link className="text-button-sm text-white border-b-2 border-white mt-4 inline-block hover:border-black hover:text-black duration-300" href="/company/about-us">Join us today!</Link>
-                        </div>
-                    </div>
-                    <div className="counter grid md:grid-cols-4 grid-cols-2 gap-y-6 md:pt-10 pt-8">
-                        <div className="counter-item px-5 border-l border-white">
-                            <div className="count-number heading3 text-white">20</div>
-                            <div className="body1 mt-1 text-white">Years experiences</div>
-                        </div>
-                        <div className="counter-item px-5 border-l border-white">
-                            <div className="flex items-center">
-                                <div className="count-number heading3 text-white">1.8</div>
-                                <span className="heading3 text-white">k</span>
-                            </div>
-                            <div className="body1 mt-1 text-white">Happy customers</div>
-                        </div>
-                        <div className="counter-item px-5 border-l border-white">
-                            <div className="count-number heading3 text-white">460</div>
-                            <div className="body1 mt-1 text-white">Project completed</div>
-                        </div>
-                        <div className="counter-item px-5 border-l border-white">
-                            <div className="count-number heading3 text-white">15</div>
-                            <div className="body1 mt-1 text-white">Awards achievement</div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="about-block">
+      <div className="container">
+        <div className="content rounded-2xl bg-linear-blue md:p-10 p-7">
+          <div className="heading flex max-lg:flex-col gap-y-4 md:pb-10 pb-8 border-b border-line">
+            <div className="w-full xl:w-5/12 lg:w-1/2">
+              <div className="tag text-label bg-blue text-white">
+                Lite kort om Telt.
+              </div>
+              <h3 className="heading3 text-white mt-4">
+                Vi Älskar Utmaningar.
+              </h3>
             </div>
-        </section>
-    )
-}
-export default AboutOne
+            <div className="w-full lg:w-1/2">
+              <div className="desc text-white">
+                Vi hjälper dig att skapa en hemsida och säkerställer att du syns
+                på Google med effektiva SEO-strategier. Låt oss hjälpa dig att
+                nå ut till fler kunder.{" "}
+              </div>
+              <Link
+                className="text-button-sm text-white border-b-2 border-white mt-4 inline-block hover:border-black hover:text-black duration-300"
+                href="/company/about-us"
+              >
+                Starta nu!
+              </Link>
+            </div>
+          </div>
+          <div className="counter grid md:grid-cols-4 grid-cols-2 gap-y-6 md:pt-10 pt-8">
+            <div className="counter-item px-5 border-l border-white">
+              <div className="count-number heading3 text-white">10</div>
+              <div className="body1 mt-1 text-white">Års erfarenhet</div>
+            </div>
+            <div className="counter-item px-5 border-l border-white">
+              <div className="flex items-center">
+                <div className="count-number heading3 text-white">700</div>
+                <span className="heading3 text-white">+</span>
+              </div>
+              <div className="body1 mt-1 text-white">Projekt slutförda</div>
+            </div>
+            <div className="counter-item px-5 border-l border-white">
+              <div className="count-number heading3 text-white">460</div>
+              <div className="body1 mt-1 text-white">Project completed</div>
+            </div>
+            <div className="counter-item px-5 border-l border-white">
+              <div className="count-number heading3 text-white">15</div>
+              <div className="body1 mt-1 text-white">Awards achievement</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default AboutOne;
--- src/components/Sections/Slider/SliderOne.js
+++ src/components/Sections/Slider/SliderOne.js
@@ -1,7 +1,6 @@
 import React from "react";
 import classNames from "classnames";
 import Link from "next/link";
-import Image from "next/image";
 import Styles from "./SliderOne.module.css";
 
 export default function SliderOne({ data, className }) {
@@ -11,22 +10,27 @@ export default function SliderOne({ data, className }) {
     >
       <div className="container">
         <div className="flex max-xl:flex-col-reverse gap-8">
-          <div className="xl:w-1/3 w-full xl:pr-[15px]">
+          <div className="xl:w-2/3 w-full xl:pr-[15px]">
             <div className="text">
               <h2 className="heading2 xl:pr-[55px]">
-                IT services for small to mid-Sized companies
+                Vi tar hand om dina IT-behov så att du kan fokusera på det som
+                verkligen räknas.
               </h2>
               <div className="body2 mt-4">
-                We provide custom software solutions for any industry. Creating
-                high - value software and technology for your business.
+                Vi bygger allt efter dina behov och hjälper dig med
+                marknadsföringen. Få skräddarsydda mjukvarulösningar och
+                expertstöd.
               </div>
             </div>
             <div className="block-button xl:mt-10 mt-6 flex gap-4 flex-wrap">
               <Link className="button-main" href="/company/about-us">
-                Get a Free Assessment
+                Få offert
               </Link>
-              <Link className="button-main bg-grey" href="/pages/contact-us">
-                Contact
+              <Link
+                className={`${Styles.contactButton} button-main bg-grey`}
+                href="/pages/contact-us"
+              >
+                Kontakta oss
               </Link>
             </div>
           </div>
